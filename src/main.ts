5dcdd40ef40792485b90d921ec329c7d1f2615b4
florian <bessonflorian42@yahoo.fr>
2025-04-07 15:58:10 +0200

    progress

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let game_container = document.getElementById("game-container");
+let game_board = document.getElementById("game-board");
+let ball = document.getElementById("ball");
+let paddle_left = document.getElementById("paddle_left");
+let paddle_right = document.getElementById("paddle_right");
+let score_left = document.getElementById("score-left");
+let score_right = document.getElementById("score-right");
+
+console.log(game_container, game_board, ball, paddle_left, paddle_right, score_left, score_right);
