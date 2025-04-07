08465863c16f62a9af63978a8e60a0b931b7f9e5
florian <bessonflorian42@yahoo.fr>
2025-04-07 17:38:17 +0200

    progress2

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,51 @@ let score_left = document.getElementById("score-left");
 let score_right = document.getElementById("score-right");
 
 console.log(game_container, game_board, ball, paddle_left, paddle_right, score_left, score_right);
+
+//variable globale avec valeur default const
+const gameHeight:number = 400;
+const gameWidth:number = 800;
+const ballSize:number = 20;
+const paddleHeight:number = 160;
+const paddleWidht:number = 10;
+
+type GameState = {
+    ballX:number;
+    ballY:number;
+    ballSpeedX:number;
+    ballSpeedY:number;
+    paddleLeftY:number;
+    paddleRightY:number;
+    scoreRight:number;
+    scoreLeft:number;
+}
+
+type Keys = {
+    w:boolean;
+    s:boolean;
+    arrowUp:boolean;
+    arrowDown:boolean;
+}
+
+//setup tout sur false
+let keys: Keys = {
+    w: false,
+    s: false,
+    arrowUp: false,
+    arrowDown: false
+}
+
+//init les valeurs avec pong de base
+let gameState: GameState = {
+    ballX:  390,
+    ballY: 190,
+    ballSpeedX: 5,
+    ballSpeedY:  2,
+    paddleLeftY: 160,
+    paddleRightY: 160,
+    scoreRight: 0,
+    scoreLeft: 0
+}
+
+function pressInput(): void
+
